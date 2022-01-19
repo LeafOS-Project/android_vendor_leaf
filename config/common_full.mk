2fa1b2b65131fc7ceb6f3638113c6e353076893a
Michael Bestas <mkbestas@gmail.com>
2022-01-20 00:17:18 +0200

    config: Build font overlays again
    Change-Id: Ibf6ea9645bbe352a430636c10c3e6e91ac79b99c

--- config/common_full.mk
+++ config/common_full.mk
@@ -13,3 +13,13 @@
 # limitations under the License.
 
 $(call inherit-product, vendor/leaf/config/common_mobile.mk)
+
+# Include {Lato,Rubik} fonts
+$(call inherit-product-if-exists, external/google-fonts/lato/fonts.mk)
+$(call inherit-product-if-exists, external/google-fonts/rubik/fonts.mk)
+
+# Fonts
+PRODUCT_PACKAGES += \
+    fonts_customization.xml \
+    FontLatoOverlay \
+    FontRubikOverlay
