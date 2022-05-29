094420462b12d7fe6f22e62a85c5d63b70b51d13
Tim Zimmermann <tim@linux4.de>
2022-05-29 20:19:53 +0200

    leaf: Set version properties
    Change-Id: I13272c3fc5c4e49604cc556c29c0791076827a33

---
+++ config/version.mk
@@ -0,0 +1,42 @@
+# Copyright (C) 2022 The LeafOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+PRODUCT_VERSION_MAJOR := 1
+PRODUCT_VERSION_MINOR := 1
+
+LEAF_BUILD_DATE := $(shell date -u +%Y%m%d)
+
+# If unset set LEAF_BUILDTYPE to the env variable RELEASE_TYPE, or
+# if that doesn't exist to "UNOFFICIAL"
+ifndef LEAF_BUILDTYPE
+    ifdef RELEASE_TYPE
+        LEAF_BUILDTYPE := $(RELEASE_TYPE)
+    else
+        LEAF_BUILDTYPE := UNOFFICIAL
+    endif
+endif
+
+ifeq ($(LEAF_BUILDTYPE), UNOFFICIAL)
+    ifneq ($(TARGET_UNOFFICIAL_BUILD_ID),)
+        LEAF_EXTRAVERSION := -$(TARGET_UNOFFICIAL_BUILD_ID)
+    endif
+endif
+
+LEAF_VERSION_SUFFIX := $(LEAF_BUILD_DATE)-$(LEAF_BUILDTYPE)$(LEAF_EXTRAVERSION)
+
+# Internal version
+LEAF_VERSION := $(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR)-$(LEAF_VERSION_SUFFIX)
+
+# Display version
+LEAF_DISPLAY_VERSION := $(LEAF_VERSION)
