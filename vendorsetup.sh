6711fa52d471db8766715fdd73ed5fba1ac00788
Tim Zimmermann <tim@linux4.de>
2022-10-04 10:21:13 +0200

    leaf: Add fetch_device command
    Change-Id: I9be76c444bb3058855a13d640a21140a4f925521

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -27,3 +27,24 @@ function leaf_lunch()
     export LINEAGE_BUILD=$LEAF_BUILD
 }
 alias lunch=leaf_lunch
+
+function fetch_device()
+{
+    if [ -z "$1" ]; then
+        echo "Usage: fetch_device <codename>"
+        return 1
+    fi
+
+    if [ ! -f "$(gettop)/leaf/devices/${1}.xml" ]; then
+        echo "Device $1 not found!"
+        return 1
+    fi
+
+    mkdir -p "$(gettop)/.repo/local_manifests"
+    cp "$(gettop)/leaf/devices/${1}.xml" "$(gettop)/.repo/local_manifests"
+
+    echo "Syncing device repos..."
+    for REPO in $(cat "$(gettop)/leaf/devices/${1}.xml" | grep '<project path=' | cut -f2 -d '"'); do
+        repo sync $REPO
+    done
+}
