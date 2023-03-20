236d606adb97a2895a7584fd5c5aa571ea8cc45a
Tim Zimmermann <tim@linux4.de>
2023-03-20 20:23:41 +0100

    leaf: Initial adblock setup
    Change-Id: I9574f5f56ba58ba604c9da84e56080a6e528832b

--- config/common.mk
+++ config/common.mk
@@ -15,6 +15,11 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/window_extensions.mk)
 $(call inherit-product-if-exists, vendor/extra/product.mk)
 
+# Adblock
+PRODUCT_COPY_FILES += \
+    vendor/leaf/etc/hosts.adblock:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/hosts.adblock \
+    vendor/leaf/etc/init/init.adblock.rc:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/init/init.adblock.rc
+
 # Apps
 PRODUCT_PACKAGES += \
     Etar \
