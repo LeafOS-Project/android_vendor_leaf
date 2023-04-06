d0ba7b3ac771225e39810e3b392de8532c7800db
SamarV-121 <samarvispute121@pm.me>
2023-04-06 16:37:31 +0530

    fetch_device: Update the script
     * Refactor the code
     * Add checks to prevent adding duplicate entries
     * Add support for listing all supported devices
    
    Change-Id: Iac037e38ba3a6f05afe8fb9c49bd519fb53e2303

--- tools/fetch_device.py
+++ tools/fetch_device.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 #
-# Copyright (C) 2022 LeafOS Project
+# Copyright (C) 2022-2023 LeafOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -16,54 +16,90 @@
 #
 
 from __future__ import print_function
-
+import argparse
+import glob
 import os
-import sys
 import yaml
-from xml.etree import ElementTree
+import xml.etree.ElementTree as ET
 
-target_device = sys.argv[1]
-topdir = sys.argv[2]
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("target_device", nargs="?", help="Target device")
+    parser.add_argument("-l", "--list", help="List all supported devices", action="store_true")
+    parser.add_argument("-t", "--topdir", help="Set the path to the source code directory")
+    return parser.parse_args()
 
-if not target_device:
-	print('Usage: fetch_device <codename>')
-	sys.exit(1)
+def load_devices(leaf_devices):
+    with open(leaf_devices) as f:
+        return yaml.safe_load(f)
 
-os.makedirs(f'{topdir}/.repo/local_manifests', exist_ok=True)
+def get_device_info(devices, args):
+    if args.list:
+        for item in devices:
+            for device in item["device"]:
+                print(device)
+        return None, None
 
-with open(f'{topdir}/leaf/devices/devices.yaml') as leaf_devices:
-	data = yaml.safe_load(leaf_devices)
-	for item in data:
-		for device in item['device']:
-			if device == target_device:
-				repositories = item['repositories']
-				family = item['family']
+    for item in devices:
+        if args.target_device in item["device"]:
+            return item["repositories"], item["family"]
 
-if not 'repositories' in locals():
-	print('Device not found')
-	sys.exit(1)
+    print("Device not found")
+    return None, None
 
-lm = ElementTree.Element('manifest')
+def repo_is_in_manifest(local_manifests, repo_path):
+    for path in glob.glob(f"{local_manifests}/*.xml"):
+        tree = ET.parse(path).getroot()
 
-for repo in repositories:
-	repo_name = repo['name']
-	repo_path = repo['path']
+        if any(localpath.get("path") == repo_path for localpath in tree.findall("project")):
+            return True
 
-	project = ElementTree.Element('project', attrib = { 'path': repo_path, 'name': repo_name })
-	if 'revision' in repo:
-		project.set('revision', repo['revision'])
-	if 'remote' in repo:
-		project.set('remote', repo['remote'])
+def update_local_manifest(local_manifest, repositories):
+    local_manifests = os.path.dirname(local_manifest)
+    os.makedirs(local_manifests, exist_ok=True)
 
-	lm.append(project)
+    try:
+        tree = ET.parse(local_manifest).getroot()
+    except:
+        tree = ET.Element("manifest")
 
-ElementTree.indent(lm, space='  ', level=0)
+    for repo in repositories:
+        repo_name = repo["name"]
+        repo_path = repo["path"]
+        if repo_is_in_manifest(local_manifests, repo_path):
+            continue
+        project = ET.Element("project", attrib={"path": repo_path, "name": repo_name})
+        if "revision" in repo:
+            project.set("revision", repo["revision"])
+        if "remote" in repo:
+            project.set("remote", repo["remote"])
+        tree.append(project)
 
-with open(f'{topdir}/.repo/local_manifests/{family}.xml', 'w') as local_manifest:
-	local_manifest.write(ElementTree.tostring(lm).decode())
+    ET.indent(tree, space="  ", level=0)
 
-for repo in repositories:
-	repo_path = repo['path']
-	print(f'Syncing {repo_path}')
-	os.system(f'repo sync --force-sync {repo_path}')
-	print('\n')
+    with open(local_manifest, "w", encoding="utf-8") as f:
+        f.write(ET.tostring(tree, encoding="unicode"))
+
+def sync_repositories(topdir, repositories):
+    for repo in repositories:
+        repo_path = repo["path"]
+        print(f"Syncing {repo_path}")
+        os.system(f"repo sync --force-sync {topdir}/{repo_path}")
+        print("\n")
+
+def main():
+    args = parse_args()
+    topdir = args.topdir or os.getcwd()
+    leaf_devices = f"{topdir}/leaf/devices/devices.yaml"
+
+    devices = load_devices(leaf_devices)
+    repositories, family = get_device_info(devices, args)
+
+    if repositories:
+        local_manifests = f"{topdir}/.repo/local_manifests"
+        local_manifest = f"{local_manifests}/{family}.xml"
+        update_local_manifest(local_manifest, repositories)
+        sync_repositories(topdir, repositories)
+
+if __name__ == "__main__":
+    main()
