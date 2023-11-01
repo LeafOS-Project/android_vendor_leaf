3b7132b7c41f954474ab0a496575179f309f482c
Tim Zimmermann <tim@linux4.de>
2023-11-01 20:25:09 +0100

    leaf: Import sensitive_pn.xml
    * From https://github.com/LineageOS/android_vendor_lineage/blob/002d21468b41ca174c953419e241a2bd0186e035/prebuilt/common/etc/sensitive_pn.xml
    
    Change-Id: Ib6d2ac7c5a7768b76113dc976bf55c3ae5bc7563

--- config/telephony.mk
+++ config/telephony.mk
@@ -12,6 +12,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# Sensitive Phone Numbers list
+PRODUCT_PACKAGES += \
+    sensitive_pn.xml
+
 # Telephony packages
 PRODUCT_PACKAGES += \
     messaging \
