61d63aeb33e8770c2ac2f0cd4fc17efe30d8ddaf
Tim Zimmermann <tim@linux4.de>
2024-05-20 20:57:14 +0200

    leaf: Include needed makefiles from config.mk
    * So that we can symlink it to vendor/google/build/config.mk for AOSP to pick it up
    
    Change-Id: I920d00baf7f677a3fd1c6ee31746ff3de23a18f5

--- build/core/config.mk
+++ build/core/config.mk
@@ -12,7 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+include vendor/leaf/config/BoardConfigLeaf.mk
 include vendor/leaf/build/core/definitions.mk
+include vendor/leaf/build/core/main.mk
 include vendor/leaf/build/core/pathmap.mk
 include vendor/leaf/build/core/qcom_target.mk
 
--- build/core/main.mk
+++ build/core/main.mk
@@ -1,4 +1,4 @@
-# Copyright (C) 2022 The LeafOS Project
+# Copyright (C) 2022-2024 The LeafOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -24,5 +24,3 @@ ADDITIONAL_PRODUCT_PROPERTIES += \
 # LeafOS Platform Display Version
 ADDITIONAL_PRODUCT_PROPERTIES += \
     ro.leaf.display.version=$(LEAF_DISPLAY_VERSION)
-
-include vendor/leaf/build/core/config.mk
