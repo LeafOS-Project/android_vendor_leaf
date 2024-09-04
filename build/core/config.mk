099c45dea8847e1e440cb70b22fd93ec5d3e27bc
Tim Zimmermann <tim@linux4.de>
2024-09-04 20:15:31 +0200

    leaf: Move setting version props to config.mk
    * ADDITIONAL_PRODUCT_PROPERTIES is already readonly when main.mk is included
    
    Change-Id: I2de7d2d5ddb6eaeecdfa276ea1a895bbba0e76fd

--- build/core/config.mk
+++ build/core/config.mk
@@ -26,3 +26,16 @@ PRODUCT_ENFORCE_PACKAGES_EXIST_ALLOW_LIST := \
     Calendar \
     Gallery2 \
     QuickSearchBox
+
+# LeafOS System Version
+ADDITIONAL_PRODUCT_PROPERTIES += \
+    ro.leaf.version=$(LEAF_VERSION) \
+    ro.leaf.releasetype=$(LEAF_BUILDTYPE) \
+    ro.leaf.flavor=$(LEAF_FLAVOR) \
+    ro.leaf.build.date=$(LEAF_BUILD_DATE) \
+    ro.leaf.build.version=$(LEAF_VERSION) \
+    ro.modversion=$(LEAF_VERSION)
+
+# LeafOS Platform Display Version
+ADDITIONAL_PRODUCT_PROPERTIES += \
+    ro.leaf.display.version=$(LEAF_DISPLAY_VERSION)
