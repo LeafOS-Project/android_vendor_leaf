125e3a4056d35edcbe753b3e748324ba42ba6373
Tim Zimmermann <tim@linux4.de>
2025-01-11 19:41:11 +0100

    leaf: Switch to PRODUCT_PRODUCT_PROPERTIES for version props
    vendor/leaf/build/core/sysprop_config.mk:16: error: ADDITIONAL_PRODUCT_PROPERTIES is obsolete. Use build/soong/scripts/gen_build_prop.py instead.
    
    Change-Id: I7bbd0a5dbd4c903fb3ab48f4606ccbda67c35fa8

--- build/core/main.mk
+++ build/core/main.mk
@@ -1,4 +1,4 @@
-# Copyright (C) 2022-2024 The LeafOS Project
+# Copyright (C) 2022 The LeafOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
